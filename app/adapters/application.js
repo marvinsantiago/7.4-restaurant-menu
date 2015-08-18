161b428accf384ff4d0cc09d58b6a9da5ac5ece1
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-17 19:22:25 -0500

    Corrected URL for host in application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   namespace: 'collections',
-  host: 'http://tiny-lr-herokuapp.com',
+  host: 'http://tiny-lr.herokuapp.com',
 
   pathForType: function(modelName) {
     return `ms-${modelName}`;
