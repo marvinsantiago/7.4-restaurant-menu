005b65370449efd52c2f944bb0ef00df3ea4e78a
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-20 16:19:56 -0500

    Categorized Items on menu

--- app/components/category-menu/component.js
+++ app/components/category-menu/component.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  items: [],
+
+  categories: Ember.computed('items', 'items.@each.category', 'items.@each.name', function() {
+    return this.get('items').reduce((build, item) => {
+      var obj = build.findBy('name', item.get('category'));
+
+      if(obj) {
+        obj.items.push(item);
+      } else {
+        obj = {
+          name: item.get('category'),
+          items: [item]
+        };
+        build.push(obj);
+      }
+
+      return build;
+      }, []);
+    })
 });
--- app/router.js
+++ app/router.js
@@ -12,14 +12,10 @@ Router.map(function() {
       this.route('create', {path: '/new'});
       this.route('show', {path: '/:menu-items_id'});
       this.route('edit', {path: '/:menu-items_id/edit'});
-      });
     });
-  this.route('categories');
-
-  this.route('menus', {path: '/menus'}, function() {
-    this.route('index', {path: '/fullmenu'});
-    this.route('show');
   });
+
+  this.route('menus', {path: '/'});
 
   });
 
